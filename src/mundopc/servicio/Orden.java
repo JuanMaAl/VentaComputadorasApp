bfde7e667c3251a719a2098059feb70341bac332
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-09 14:09:26 +0200

    clase Orden

--- src/mundopc/servicio/Orden.java
+++ src/mundopc/servicio/Orden.java
@@ -1,4 +1,30 @@
 package mundopc.servicio;
+import mundopc.modelo.Computadora;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Orden {
-}
+    //Atributos
+    private final int idOrden;
+    private final List<Computadora> computadoras;
+    private static int contadorOrdenes;
+
+    //Constructores
+    public Orden(){
+        computadoras = new ArrayList<>();
+        idOrden = ++contadorOrdenes;
+    }
+
+    //Metódos
+    public void agregarComputadora(Computadora computadora){
+        computadoras.add(computadora);
+    }
+
+    public void mostrarOrden(){
+        System.out.println("Orden #: " + idOrden);
+        System.out.println("Total de computadoras: " + computadoras.size());
+        System.out.println();
+        computadoras.forEach(System.out::println);
+    }
+
+}//fin clase Orden
