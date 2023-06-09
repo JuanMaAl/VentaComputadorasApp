2f8d47bf0af5123c3d02aa4e92fd215089ecedd4
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-09 06:31:23 +0200

    Iniciando el proyecto, package mundopc y package modelo

---
+++ src/mundopc/Main.java
@@ -0,0 +1,13 @@
+package mundopc;
+
+/*
+    Segundo proyecto del curso "Java en 13 Días con Aplicaciones del Mundo Real!"
+    Aprende Java, Spring Boot, Swing, Java FX, JSPs, Thymeleaf, JSF-PrimeFaces,
+    FullStack con Angular, React y Spring Boot!
+    https://www.udemy.com/course/aprende-java-en-13-dias-con-aplicaciones-del-mundo-real-spring/
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }// fin main
+}// fin clase mundopc.Main
