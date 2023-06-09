cef19936696ed6fba41f98ac92fc8daa2f1d7581
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-09 11:12:57 +0200

    Clase Teclado

---
+++ src/mundopc/modelo/Teclado.java
@@ -0,0 +1,22 @@
+package mundopc.modelo;
+
+public class Teclado extends DispositivoEntrada{
+    //Atributos
+    private final int idTeclado;
+    private static int contadorTeclados = 0;
+
+    //Constructor
+    public Teclado(String tipoEntrada, String marca){
+        super(tipoEntrada, marca);
+        idTeclado = ++contadorTeclados;
+    }
+
+    //To String
+
+    @Override
+    public String toString() {
+        return "Teclado{" +
+                "idTeclado=" + idTeclado +
+                "} " + super.toString();
+    }
+}
