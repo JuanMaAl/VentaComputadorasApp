9c2b996b668ea08e60dadab11e1a3511db28c084
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-09 13:03:51 +0200

    creación de un ratón, un teclado y un monitor. Funciona

--- src/mundopc/Main.java
+++ src/mundopc/Main.java
@@ -1,5 +1,9 @@
 package mundopc;
 
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
 /*
     Segundo proyecto del curso "Java en 13 Días con Aplicaciones del Mundo Real!"
     Aprende Java, Spring Boot, Swing, Java FX, JSPs, Thymeleaf, JSF-PrimeFaces,
@@ -8,6 +12,13 @@ package mundopc;
  */
 public class Main {
     public static void main(String[] args) {
+        //Crear objetos
+        Raton ratonLenovo = new Raton("bluetooth", "Lenovo");
+        System.out.println(ratonLenovo);
+        Teclado tecladoLenovo = new Teclado("bluetooth", "Lenovo");
+        System.out.println(tecladoLenovo);
+        Monitor monitorLenovo = new Monitor("Lenovo", 27);
+        System.out.println(monitorLenovo);
 
     }// fin main
 }// fin clase mundopc.Main
